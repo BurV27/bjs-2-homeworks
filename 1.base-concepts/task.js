90e01af1c11e0532ce2cb1917c3c40dcac074e88
Vladislav <vbura2000@gmail.com>
2025-04-22 14:58:03 +0500

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let result = (Math.pow(b, 2) - 4 * a * c);
+	if (result === 0) {
+		arr[0] = (-b / (2 * a));
+	} else if (result > 0) {
+		arr[0] = (-b + Math.sqrt(result)) / (2 * a);
+		arr[1] = (-b - Math.sqrt(result)) / (2 * a);
+	} else {
+		arr = []
+	}
+	return console.log(arr);
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let transformPercent = (percent / 100) / 12;
+	let bodyCredit = amount - contribution;
+	let payment = bodyCredit * (transformPercent + (transformPercent / ((Math.pow((1 + transformPercent), countMonths) - 1))));
+	let totalAmount = payment * countMonths
+	return console.log(totalAmount.toFixed(2))
 }
