04b7a7a92a7ca588bffbe7edabda51636974baa3
Bura Vladislav Vyacheslavovich <vlbura@o3.ru>
2025-04-27 11:40:30 +0500

    3 homeworks

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.every(elem => elem === arr2[elem] && arr1.length === arr2.length) 
 }
 
+
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+    return users.
+    filter(position => position.gender === gender)
+    .map(ages => ages.age)
+    .reduce((acc, item, index, array) => acc + item / array.length, 0)
 }
