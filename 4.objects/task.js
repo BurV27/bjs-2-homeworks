b73a8808e6dd76a427ddaa761f415b9983d3bce7
Bura Vladislav Vyacheslavovich <vlbura@o3.ru>
2025-05-05 11:07:01 +0500

    Подгрузка 5 задания и редактирование 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+    this.name = name
+    this.gender = gender
+    this.age = age;
+    this.marks = []
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if(!this.marks){
+        return 
+    }
+    this.marks.push(...marks)
 }
 
 Student.prototype.getAverage = function () {
-  
+    if(!this.marks){
+        return 0
+    }
+    let sum = 0;
+    for (let element of this.marks){
+        sum += element
+        console.log(sum)
+    }
+    return sum / this.marks.length
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject
+    delete this.marks
+    this.excluded = reason;
 }
